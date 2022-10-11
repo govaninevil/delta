a51768451c72bb63de42e04de662cbca72add39f
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2022-10-11 04:12:46 -0700

    main

--- script/script.js
+++ script/script.js
@@ -6,31 +6,4 @@ $(document).on('ready', function () {
       $(this).addClass("active");
       $(".nav-box li").not(this).removeClass("active");
     });
-
-    $('select').change(function(){
-      if($('select option:selected').text() == "Conformation Number"){
-      $('.confermation-number').show();
-      }
-      else{
-      $('.confermation-number').hide();
-      }
-    });
-
-    $('select').change(function(){
-      if($('select option:selected').text() == "Credit/Debit Card Number"){
-      $('.card-number').show();
-      }
-      else{
-      $('.card-number').hide();
-      }
-    });
-
-    $('select').change(function(){
-      if($('select option:selected').text() == "Ticket Number"){
-      $('.ticket-number').show();
-      }
-      else{
-      $('.ticket-number').hide();
-      }
-    });
 });
